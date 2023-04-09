cad30e5984af5fcc72b1bda7e609b0a30d35175c
mike-shields-dev <mr.m.p.shields@gmail.com>
2023-04-10 00:05:43 +0100

    Add graphing perlin noise example

---
+++ graphing-perlin-noise/README.md
@@ -0,0 +1,3 @@
+#Graphing Perlin Noise
+
+The sketch demonstrates how to scan through the Perlin Noise values seeded by the `noise` function in P5js and plot the samples along the x-axis of a simple graph.
---
+++ graphing-perlin-noise/sketch.js
@@ -0,0 +1,59 @@
+let range = 0.01;
+let rangeSlider;
+let travel = 0.01;
+let travelSlider;
+let gain = 1;
+let gainSlider;
+let start = 0;
+
+function setup() {
+  rangeSlider = document.querySelector('#range-slider');
+  rangeSlider.min = 0;
+  rangeSlider.max = 1;
+  rangeSlider.step = 0.0001;
+  rangeSlider.value = 0.01;
+
+  rangeSlider.addEventListener('input', e => {
+    range = +e.target.value
+  });
+
+  travelSlider = document.querySelector('#travel-slider');
+  travelSlider.min = 0;
+  travelSlider.max = 1;
+  travelSlider.step = 0.001;
+  travelSlider.value = 0.01;
+
+  travelSlider.addEventListener('input', e => {
+    travel = +e.target.value
+  });
+
+  gainSlider = document.querySelector('#gain-slider');
+  gainSlider.min = 0;
+  gainSlider.max = 1;
+  gainSlider.step = 0.0001;
+  gainSlider.value = 1;
+
+  gainSlider.addEventListener('input', e => {
+    gain = +e.target.value;
+  });
+
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(0);
+  stroke(255);
+  noFill();
+  beginShape()
+
+  let xOffset = start;
+
+  for (let x = 0; x < width; x++) {
+    const y = map(noise(xOffset), 0, 1, -height * gain, height * gain) + 200;
+    vertex(x, y);
+    xOffset += range;
+  }
+  endShape();
+
+  start += travel;
+}
