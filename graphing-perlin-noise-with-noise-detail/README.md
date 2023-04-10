36f94aac7ebdfb4e13576fbee9fef77473127993
mike-shields-dev <mr.m.p.shields@gmail.com>
2023-04-10 10:56:39 +0100

    Add noise detail examples

---
+++ graphing-perlin-noise-with-noise-detail/README.md
@@ -0,0 +1,5 @@
+#Graphing Perlin Noise
+
+The sketch demonstrates how to graph regions of the Perlin noise field by sampling values from different regions along the Perlin noise x-axis and plotting them along the x-axis of a 2D canvas.  
+
+The noise detail function allows the quality of the Perlin noise field to be changed. 
---
+++ graphing-perlin-noise-with-noise-detail/sketch.js
@@ -0,0 +1,89 @@
+let range = 0.01;
+let rangeSlider;
+
+let travel = 0.01;
+let travelSlider;
+
+let gain = 0.5;
+let gainSlider;
+
+let detail = 1;
+let detailSlider;
+
+let fallOff = 0.5;
+let fallOffSlider;
+
+let start = 0;
+
+function setup() {
+  rangeSlider = document.querySelector('#range-slider');
+  rangeSlider.min = 0;
+  rangeSlider.max = 1;
+  rangeSlider.step = 0.0001;
+  rangeSlider.value = range;
+
+  rangeSlider.addEventListener('input', e => {
+    range = +e.target.value
+  });
+
+  travelSlider = document.querySelector('#travel-slider');
+  travelSlider.min = 0;
+  travelSlider.max = 1;
+  travelSlider.step = 0.001;
+  travelSlider.value = travel;
+
+  travelSlider.addEventListener('input', e => {
+    travel = +e.target.value
+  });
+
+  gainSlider = document.querySelector('#gain-slider');
+  gainSlider.min = 0;
+  gainSlider.max = 1;
+  gainSlider.step = 0.0001;
+  gainSlider.value = gain;
+
+  gainSlider.addEventListener('input', e => {
+    gain = +e.target.value;
+  });
+
+  detailSlider = document.querySelector('#detail-slider');
+  detailSlider.min = 0;
+  detailSlider.max = 32;
+  detailSlider.step = 1;
+  detailSlider.value = detail;
+
+  detailSlider.addEventListener('input', e => {
+    detail = +e.target.value;
+  });
+
+  fallOffSlider = document.querySelector('#falloff-slider');
+  fallOffSlider.min = 0;
+  fallOffSlider.max = 1;
+  fallOffSlider.step = 0.0001;
+  fallOffSlider.value = fallOff;
+
+  fallOffSlider.addEventListener('input', e => {
+    fallOff = +e.target.value;
+  });
+
+  createCanvas(400, 400);
+}
+
+function draw() {
+  noiseDetail(detail, fallOff);
+  background(0);
+  stroke(255);
+  noFill();
+  beginShape()
+
+  let xOffset = start;
+
+  for (let x = 0; x < width; x++) {
+    const y = map(noise(xOffset), 0, 1, -height * gain, height * gain) + 200;
+    vertex(x, y);
+    xOffset += range;
+  }
+  endShape();
+
+  start += travel;
+}
